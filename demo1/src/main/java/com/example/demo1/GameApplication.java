d9eae5017bc3a10e97b1c10af28bcc1fb412c78d
lKReed <86077259+lKReed@users.noreply.github.com>
2022-01-27 00:45:37 -0500

    Made a start screen with image background and button that cycles to a new stage
    ~ L.R.

--- demo1/src/main/java/com/example/demo1/GameApplication.java
+++ demo1/src/main/java/com/example/demo1/GameApplication.java
@@ -2,13 +2,16 @@ package com.example.demo1;
 
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -22,54 +25,31 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.FontPosture;
 import javafx.scene.image.Image;
 
+import java.io.FileInputStream;
 import java.util.Stack;
 
 public class GameApplication extends Application{//PLEASE put the opening braces on the top next time NAT >:( ~NS :)
     @Override
-    public void start(Stage theStage){
-        Button startButton = new Button("Start");
-        startButton.setLayoutX(100);
-        startButton.setLayoutY(80);
-        Text startText = new Text("START HERE PLAYER");
-        startText.setLayoutX(500);
-        startText.setLayoutY(353);
-        Image startImagePNG = new Image( "SOIMainScreen.png" );
-        ImageView startImage = new ImageView(startImagePNG);
-        TextField startTextField = new TextField();
-        startTextField.setEditable(true);
+    public void start(Stage theStage) throws Exception {
 
+        Button startButton = new Button("START");
+        startButton.setMinSize(200, 50);
+        // startButton.setStyle("-fx-border-color: black; -fx-text-fill: black; -fx-border- width: 3px; -fx-font-size: 30px;");
 
+        FileInputStream stream = new FileInputStream("C:\\Users\\lkree\\IdeaProjects\\SOI\\SOIMainScreen.png");
+        Image image = new Image(stream);
+        ImageView imageview = new ImageView(image);
 
+        StackPane layout = new StackPane(imageview, startButton);
+        StackPane layout2 = new StackPane();
+        Scene scene = new Scene(layout, 1280, 720);
+        Scene scene2 = new Scene(layout2, 1280, 720);
 
-        //Button Action
-
-
-
-        HBox textAndButton = new HBox();
-        textAndButton.getChildren().addAll(startButton , startTextField);
-
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(startImage, textAndButton);
-
-        Group root = new Group(stackPane);
-
-        theStage.setTitle( "The Sorceress of Isan" );
-        Scene theScene = new Scene(root);
-
-        theStage.setScene(theScene);
-
+        theStage.setTitle("The Sorceress of Isan");
+        theStage.setScene(scene);
         theStage.show();
-/*      THIS IS WHERE I START TO TRY TO MAKE THE NEW SCENES BUT IT STARTS ERRORS
-        Group root2 = new Group(startText);
-        Scene theScene2 = new Scene(root2);
-        theStage.setScene(theScene2);
-        startButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                startButton.setText("THIS BUTTON IS PRESSED");
-                theStage.show();
-            }
-        });*/
+
+        startButton.setOnAction(e -> theStage.setScene(scene2));
     }
 
     public static void main(String[] args){
