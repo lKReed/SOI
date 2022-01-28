09ac5f1b505b0be36479f38e2b037010113b58d8
lKReed <86077259+lKReed@users.noreply.github.com>
2022-01-28 18:15:48 -0500

    Made base of dialogue screen, not pulling text from files or having buttons doing anything yet - having bug with buttons, see trello for details, doesn't impact it running though.
    ~ L.R.

--- demo1/src/main/java/com/example/demo1/GameApplication.java
+++ demo1/src/main/java/com/example/demo1/GameApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo1;
 
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -26,42 +25,59 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.FontPosture;
 import javafx.scene.image.Image;
-
 import java.io.FileInputStream;
 import java.util.Stack;
 
-public class GameApplication extends Application{//PLEASE put the opening braces on the top next time NAT >:( ~NS :)
+public class GameApplication extends Application{
     @Override
     public void start(Stage theStage) throws Exception {
 
+        // start button declaration
         Button startButton = new Button("START");
         startButton.setMinSize(200, 50);
-        // startButton.setStyle("-fx-border-color: black; -fx-text-fill: black; -fx-border- width: 3px; -fx-font-size: 30px;");
 
+        // dialogue button declaration - there is a way to change button text, so we'll likely do that to present dialogue options when we get there
+        Button dialogueOption1 = new Button("1");
+        Button dialogueOption2 = new Button("2");
+        Button next = new Button("Next");
+
+        // start screen background
         FileInputStream stream = new FileInputStream("C:\\Users\\lkree\\IdeaProjects\\SOI\\SOIMainScreen.png");
         Image image = new Image(stream);
         ImageView startImage = new ImageView(image);
         startImage.setPreserveRatio(true);
-/*
-        GridPane resizeLayout = new GridPane();
-        resizeLayout.setPadding(new Insets(10, 10, 10, 10));
-        resizeLayout.setVgap(5);
-        resizeLayout.setHgap(5);
-*/
-        StackPane layout = new StackPane(startImage, startButton);
-        StackPane layout2 = new StackPane();
 
-       // resizeLayout.add(layout, 0, 0);
+        // dialogue screen text - need help figuring out files and stuff to pull dialogue from there
+        Text text = new Text("Testing testing 123");
 
-        Scene scene = new Scene(layout, 1280, 720);
-        Scene scene2 = new Scene(layout2, 1280, 720);
+        // puts dialogue buttons in a HBox so we can control where they are on the screen
+        HBox dialogueButtons = new HBox(30, dialogueOption1, dialogueOption2);
+        dialogueButtons.setAlignment(Pos.BOTTOM_CENTER);
+        HBox.setMargin(dialogueOption1, new Insets(20,20,20,20));
+        HBox.setMargin(dialogueOption2, new Insets(20,20,20,20));
 
+        // puts text in a VBox so we can control where it is on the screen
+        VBox dialogue = new VBox(30, text, next);
+        dialogue.setAlignment(Pos.CENTER);
+        HBox.setMargin(text, new Insets(20,20,20,20));
+        HBox.setMargin(next, new Insets(20,20,20,20));
+
+        // declares stackpanes
+        StackPane startLayout = new StackPane(startImage, startButton);
+        StackPane dialogueLayout = new StackPane(dialogueButtons, dialogue);
+
+        // scene declaration
+        Scene startScreen = new Scene(startLayout, 1280, 720);
+        Scene dialogueScreen = new Scene(dialogueLayout, 1280, 720);
+
+        // sets up the stage in fullscreen and shows it
         theStage.setTitle("The Sorceress of Isan");
-        theStage.setScene(scene);
+        theStage.setScene(startScreen);
         theStage.setFullScreen(true);
         theStage.show();
 
-        startButton.setOnAction(e -> {theStage.setScene(scene2); theStage.setFullScreen(true);});
+        // transitions to dialogue screen when start button is pressed - realistically there will be a scene or two before the dialogue scene, but we'll get there when we get there
+        startButton.setOnAction(e -> {theStage.setScene(dialogueScreen); theStage.setFullScreen(true);});
     }
 
     public static void main(String[] args){
